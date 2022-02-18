2025d3bd77a6d3e987fc60d882263bdae4525a6d
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-02-18 22:47:32 +0900

    get products with cache

--- src/controllers/productController.go
+++ src/controllers/productController.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"context"
+	"encoding/json"
 	"go-fiber-demo/src/database"
 	"go-fiber-demo/src/models"
 	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -63,4 +66,31 @@ func DeleteProduct(c *fiber.Ctx) error {
 	database.DB.Delete(&product)
 
 	return nil
+}
+
+func ProductsFrontend(c *fiber.Ctx) error {
+	var products []models.Product
+	var ctx = context.Background()
+
+	// attemp to get cached products
+	result, err := database.Cache.Get(ctx, "products_frontend").Result()
+
+	// there's no cached products
+	if err != nil {
+		database.DB.Find(&products)
+
+		bytes, err := json.Marshal(products)
+
+		if err != nil {
+			panic(err)
+		}
+		// caching products
+		if errKey := database.Cache.Set(ctx, "products_frontend", bytes, 30*time.Minute); errKey != nil {
+			panic(errKey)
+		}
+	} else {
+		json.Unmarshal([]byte(result), &products)
+	}
+
+	return c.JSON(products)
 }
